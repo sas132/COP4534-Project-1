3ea3b74eda846c607400c7e01c950e009a713182
Sarah Stevenson <sas132@students.uwf.edu>
2019-09-17 14:49:58 -0500

    adding basic tests for file_directory in main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,30 @@
 
 int main()
 {
+	//dir1 = directory
 	FileDirectory dir1;
 	dir1.setName("dir1");
 	std::cout << dir1.getName() << std::endl;
+
+	//fil1 = file
+	FileDirectory fil1("fil1");
+	std::cout << std::endl << fil1.getName() << std::endl << std::endl;
+
+	fil1.setParent(&dir1);
+	dir1.setChild(&fil1);
+	std::cout << "fil1's parent: " << fil1.getParent()->getName() << std::endl;
+	std::cout << "dir1's child: " << dir1.getChild()->getName() << std::endl << std::endl;
+	
+	//dir2 = directory
+	FileDirectory dir2("dir2", true);
+	dir2.setParent(&dir1);
+	dir2.setSibling(dir1.getChild());
+	dir1.setChild(&dir2);
+	std::cout << "dir2's parent: " << dir2.getParent()->getName() << std::endl;
+	std::cout << "dir2's sibling: " << dir2.getSibling()->getName() << std::endl;
+	std::cout << "dir1's child: " << dir1.getChild()->getName() << std:: endl;
+	std::cout << "fil1's parent: " << fil1.getParent()->getName() << std::endl;
+	//fil1's sibling gets segmentation fault, works properly
+	//std::cout << "fil1's sibling: " << fil1.getSibling()->getName() << std::endl << std::endl;
 	return 0;
 }
